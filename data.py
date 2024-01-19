6f4580b9e705f44c1be583757f224d8b717da9de
Haik Shougarian <haik@Haik-Shougarian-FVFHTCBEQ6L3.local>
2024-01-19 14:39:49 -0500

    Starter code

---
+++ data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 14:34:32 2024
+
+@author: haik
+"""
+
+transactions_list = [{'ID': 1, 'Data': 'Sent 5 BTC', 'Fee' : 0.1}
+                     , {'ID': 2, 'Data': 'Sent 7 BTC', 'Fee' : 0.001}
+                     , {'ID': 3, 'Data': 'Sent 0.5 BTC', 'Fee' : 0.02}
+                     , {'ID': 4, 'Data': 'Sent 0.7 BTC', 'Fee' : 0.2}
+                     , {'ID': 5, 'Data': 'Sent 1.7 BTC', 'Fee' : 0.0004}
+                     , {'ID': 6, 'Data': 'Sent 1.4 BTC', 'Fee' : 0.00375}
+                     , {'ID': 7, 'Data': 'Sent 7.3 BTC', 'Fee' : 0.001}
+                     , {'ID': 8, 'Data': 'Sent 1.67 BTC', 'Fee' : 0.002}
+                     , {'ID': 9, 'Data': 'Sent 5.7 BTC', 'Fee' : 0.09}
+                     , {'ID': 10, 'Data': 'Sent 3.7 BTC', 'Fee' : 0.12}
+                     , {'ID': 11, 'Data': 'Sent 7.5 BTC', 'Fee' : 0.33}
+                     , {'ID': 12, 'Data': 'Sent 3.1 BTC', 'Fee' : 0.23}
+                     , {'ID': 13, 'Data': 'Sent 4.6 BTC', 'Fee' : 0.24}
+                     , {'ID': 14, 'Data': 'Sent 5.2 BTC', 'Fee' : 0.048}
+                     , {'ID': 15, 'Data': 'Sent 7.5 BTC', 'Fee' : 0.08}
+                     , {'ID': 16, 'Data': 'Sent 1.37 BTC', 'Fee' : 0.4}
+                     , {'ID': 17, 'Data': 'Sent 9.1 BTC', 'Fee' : 0.06}
+                     , {'ID': 18, 'Data': 'Sent 2.5 BTC', 'Fee' : 0.000008}
+                     , {'ID': 19, 'Data': 'Sent 2.6 BTC', 'Fee' : 0.1234}
+                     , {'ID': 20, 'Data': 'Sent 2.2 BTC', 'Fee' : 0.045}
+                     , {'ID': 21, 'Data': 'Sent 6.8 BTC', 'Fee' : 0.0042}
+                     , {'ID': 22, 'Data': 'Sent 9.3 BTC', 'Fee' : 0.0001}
+                     , {'ID': 23, 'Data': 'Sent 4.3 BTC', 'Fee' : 0.008}]
--- transactionpicker.py
+++ transactionpicker.py
@@ -1,7 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# Import Libraries
+import json
+from flask import Flask, jsonify
+from data import transactions_list
 
-This is a temporary script file.
-"""
 
+
+# Create Web App
+app = Flask(__name__)
+
+
+# returns list of transactions
+@app.route('/get_transactions', methods=['GET'])
+def get_transactions():
+    response = {'Transactions': transactions_list}
+    return jsonify(response), 200
+
+
+# Returns list of 10 transactions with highest fees
+@app.route('/get_ten_highest_fees', methods=['GET'])
+def get_ten_highest_fees():
+    response = {} # placeholder
+    return jsonify(response), 200
+
+
+# Returns list of 10 transactions with lowest fees
+@app.route('/get_ten_lowest_fees', methods=['GET'])
+def get_ten_lowest_fees():
+    response = {} # placeholder
+    return jsonify(response), 200
+    
+
+
+# Returns second highest total fee sum after picking 10 transactions
+@app.route('/get_next_highest_total', methods=['GET'])
+def get_next_highest_total():
+    response = {} # placeholder
+    return jsonify(response), 200
+    
+
+
+
+# Run app
+app.run(host='0.0.0.0', port=5050)
